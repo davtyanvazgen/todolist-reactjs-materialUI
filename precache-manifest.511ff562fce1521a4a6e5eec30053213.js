cba624401054e72a4f5454cf3040cad218682100
Vazgen Davtyan <vazgenddavtyan@gmail.com>
2019-01-23 15:10:42 +0400

    Updates

---
+++ precache-manifest.511ff562fce1521a4a6e5eec30053213.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e3dada6f0ae6286ce53",
+    "url": "/todolist-reactjs-materialUI/static/js/runtime~main.0e3dada6.js"
+  },
+  {
+    "revision": "80ff6b493e8e99d1beb5",
+    "url": "/todolist-reactjs-materialUI/static/js/main.80ff6b49.chunk.js"
+  },
+  {
+    "revision": "7380b2c3e2a772f10b98",
+    "url": "/todolist-reactjs-materialUI/static/js/1.7380b2c3.chunk.js"
+  },
+  {
+    "revision": "80ff6b493e8e99d1beb5",
+    "url": "/todolist-reactjs-materialUI/static/css/main.addef249.chunk.css"
+  },
+  {
+    "revision": "709ab2af78bbeac3ba2c20d792ed06b0",
+    "url": "/todolist-reactjs-materialUI/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/todolist-reactjs-materialUI/precache-manifest.511ff562fce1521a4a6e5eec30053213.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/todolist-reactjs-materialUI/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
